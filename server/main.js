5e82894cb34bd7254a8c3064d54a5994e24903ce
aarondupon <info@aarondupon.be>
2019-07-08 11:48:41 +0200

    update

--- server/main.js
+++ server/main.js
@@ -7,12 +7,13 @@ function insertShop(data) {
 }
 
 Meteor.startup(() => {  
-  Shop.remove({})
-  meteorAPI.get(({data})=>{
-    const {records} = data;
-    records.forEach(element => {
-      insertShop(element)
-    }); 
-  })
+  /** local testing */
+  // Shop.remove({})
+  // meteorAPI.get(({data})=>{
+  //   const {records} = data;
+  //   records.forEach(element => {
+  //     insertShop(element)
+  //   }); 
+  // })
   
 });

92c7399a5c6d12aced5dea8faf8b689c52aa4b48
aarondupon <info@aarondupon.be>
2019-07-08 10:44:34 +0200

    db

--- server/main.js
+++ server/main.js
@@ -3,16 +3,16 @@ import {Shop} from '/imports/api/shop';
 import meteorAPI from '/imports/api/airtable/meteorAPI';
 
 function insertShop(data) {
-  // Shop.insert({ ...data.fields, createdAt: new Date() });
+  Shop.insert({ ...data.fields, createdAt: new Date() });
 }
 
 Meteor.startup(() => {  
-  // Shop.remove({})
-  // meteorAPI.get(({data})=>{
-  //   const {records} = data;
-  //   records.forEach(element => {
-  //     insertShop(element)
-  //   }); 
-  // })
+  Shop.remove({})
+  meteorAPI.get(({data})=>{
+    const {records} = data;
+    records.forEach(element => {
+      insertShop(element)
+    }); 
+  })
   
 });
